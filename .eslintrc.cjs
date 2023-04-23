9d22715c87c3a2dda059f28914a7051016a6bf11
liaorui <liaor@suntang.com>
2023-04-23 11:26:03 +0800

    测试

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,23 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'standard-with-typescript',
-    'plugin:prettier/recommended' // 新增
-  ],
-  overrides: [
-  ],
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    parser: '@typescript-eslint/parser',
-    project: ['tsconfig.json'], // 新增
-    extraFileExtensions: ['.vue']
-  },
-  plugins: [
-    'vue'
-  ],
-  rules: {
-  }
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'standard-with-typescript',
+        'plugin:prettier/recommended' // 新增
+    ],
+    overrides: [],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        parser: '@typescript-eslint/parser',
+        project: ['tsconfig.json'], // 新增
+        extraFileExtensions: ['.vue']
+    },
+    plugins: ['vue'],
+    rules: {}
 }
