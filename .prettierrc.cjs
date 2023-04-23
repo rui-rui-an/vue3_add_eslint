6f9cdb356de965a2a6a9c35c693a14b3f292034d
liaorui <liaor@suntang.com>
2023-04-23 11:28:38 +0800

    测试

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  tabWidth: 8, // 指定每个缩进级别的空格数<int>，默认2
+  tabWidth: 2, // 指定每个缩进级别的空格数<int>，默认2
   useTabs: false, // 用制表符而不是空格缩进行<bool>，默认false
   printWidth: 300, //一行的字符数，如果超过会进行换行，默认为80
   singleQuote: true, //字符串是否使用单引号，默认为false，使用双引号
