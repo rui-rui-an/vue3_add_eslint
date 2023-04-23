dba271148ecb325b3960322fceb01f9a588d4357
liaorui <liaor@suntang.com>
2023-04-23 11:13:26 +0800

    初次提交

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+    project: ['tsconfig.json'], // 新增
+    extraFileExtensions: ['.vue']
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
