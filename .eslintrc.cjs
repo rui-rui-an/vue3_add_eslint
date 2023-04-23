1976a15dc52bfe9c6bfae2322d9c6617386b1a13
liaorui <liaor@suntang.com>
2023-04-23 11:24:31 +0800

    测试

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,8 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
-    'standard-with-typescript'
+    'standard-with-typescript',
+    'plugin:prettier/recommended' // 新增
   ],
   overrides: [
   ],
